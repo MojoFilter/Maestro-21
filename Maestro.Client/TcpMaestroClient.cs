9473dde61a5550d91c2d54942bcd6fa0d730797d
Josh Weeks <weeks.josh@gmail.com>
2021-03-07 18:34:12 -0500

    add client gui; refactor client for concurrent bi-directionality

--- Maestro.Client/TcpMaestroClient.cs
+++ Maestro.Client/TcpMaestroClient.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,26 +15,50 @@ namespace Maestro.Client
         {
             _client = new TcpClient();
             await _client.ConnectAsync(host, port).ConfigureAwait(false);
+            this.Read();
         }
 
-        public Task<string> GetStatusAsync(CancellationToken cancellationToken = default)
+        public IObservable<bool> Status => _status.AsObservable();
+
+        public IObservable<Exception> Error => _error.AsObservable();
+
+        public Task GetStatusAsync(CancellationToken cancellationToken = default)
             => this.ExecuteAsync(Commands.GetStatus, 0x0, cancellationToken);
 
-        public Task<string> WakeAsync(CancellationToken cancellationToken = default)
+        public Task WakeAsync(CancellationToken cancellationToken = default)
             => this.ExecuteAsync(Commands.Wake, 0x0, cancellationToken);
 
-        public Task<string> SleepAsync(CancellationToken cancellationToken = default)
+        public Task SleepAsync(CancellationToken cancellationToken = default)
             => this.ExecuteAsync(Commands.Sleep, 0x0, cancellationToken);
 
-        private async Task<string> ExecuteAsync(Commands command, byte arg, CancellationToken cancellationToken)
+        private async Task ExecuteAsync(Commands command, byte arg, CancellationToken cancellationToken)
         {
             var stream = _client.GetStream();
             var message = new byte[] { (byte)command, arg };
             await stream.WriteAsync(message, 0, 2, cancellationToken).ConfigureAwait(false);
-            var reader = new StreamReader(stream, Encoding.UTF8, true, 8, true);
-            return await reader.ReadLineAsync().ConfigureAwait(false);
         }
 
+        private async void Read()
+        {
+            try
+            {
+                var stream = _client.GetStream();
+                var reader = new StreamReader(stream);
+                while (!_readCanceller.IsCancellationRequested)
+                {
+                    var message = await reader.ReadLineAsync().ConfigureAwait(false);
+                    _status.OnNext(message.StartsWith("I"));
+                }
+            }
+            catch (Exception ex)
+            {
+                _error.OnNext(ex);
+            }
+        }
+
+        private readonly ISubject<Exception> _error = new Subject<Exception>();
+        private readonly ISubject<bool> _status = new BehaviorSubject<bool>(false);
         private TcpClient _client;
+        private CancellationTokenSource _readCanceller = new CancellationTokenSource();
     }
 }
--- Maestro.Console/Program.cs
+++ Maestro.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Maestro.Client;
 using System;
 using System.Threading.Tasks;
+using Terminal = System.Console;
 
 namespace Maestro.Console
 {
@@ -10,6 +11,23 @@ namespace Maestro.Console
         {
             var client = new TcpMaestroClient();
             await client.ConnectAsync("192.168.86.63", 4321).ConfigureAwait(false);
+            
+            client.Error
+                  .Subscribe(ex =>
+                  {
+                      Terminal.ForegroundColor = ConsoleColor.Red;
+                      Terminal.WriteLine($"** {ex.Message}");
+                      Terminal.ResetColor();
+                  });
+
+            client.Status
+                  .Subscribe(isAwake =>
+                  {
+                      Terminal.ForegroundColor = ConsoleColor.Green;
+                      var stat = isAwake ? "Awake" : "Asleep";
+                      Terminal.WriteLine($"> {stat}");
+                      Terminal.ResetColor();
+                  });
             System.Console.WriteLine("Connected AF");
             bool gettin = true;
             do
@@ -17,23 +35,21 @@ namespace Maestro.Console
                 System.Console.Write("?");
                 var cmd = System.Console.ReadKey();
                 System.Console.WriteLine();
-                var response = string.Empty;
                 switch (cmd.KeyChar)
                 {
                     case 's':
-                        response = await client.GetStatusAsync().ConfigureAwait(false);
+                        await client.GetStatusAsync().ConfigureAwait(false);
                         break;
                     case '+':
-                        response = await client.WakeAsync().ConfigureAwait(false);
+                        await client.WakeAsync().ConfigureAwait(false);
                         break;
                     case '-':
-                        response = await client.SleepAsync().ConfigureAwait(false);
+                        await client.SleepAsync().ConfigureAwait(false);
                         break;
                     case 'q':
                         gettin = false;
                         break;
                 }
-                System.Console.WriteLine($"> {response}");
             } while (gettin);
         }
     }
